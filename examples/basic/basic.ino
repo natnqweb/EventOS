da5cb461a676caa408fd9f6547a27b7e7ee05adb
Natan Lisowski <82398767+natnqweb@users.noreply.github.com>
2022-10-01 00:24:14 +0200

    example :new:

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -16,7 +16,7 @@ void PrintPinStateIsFalling()
     LOGLN("Pin State is falling");
 }
 
-void PrintPinStateIsRaising()
+void PrintPinStateIsRising()
 {
     LOGLN("Pin State is Rising");
 }
@@ -25,9 +25,9 @@ PROGRAM_SETUP(115200)
 {
     pinMode(ledPin, OUTPUT);
     // this is purely implemented in software it has nothing to do with hardware interrupts
-    AddEventListener(_PIN_D2, ON_CHANGE_EVENT, ChangeLedState);
-    AddEventListener(_PIN_D2, ON_RISING_EDGE_EVENT, ChangeLedState);
-    AddEventListener(_PIN_D2, ON_FALLING_EDGE_EVENT, ChangeLedState);
+    AddEventListener(_PIN_D2, ON_CHANGE_EVENT, ChangeLedState); // 0 -> 1 or 1 -> 0
+    AddEventListener(_PIN_D2, ON_RISING_EDGE_EVENT, PrintPinStateIsRising); // 0 -> 1
+    AddEventListener(_PIN_D2, ON_FALLING_EDGE_EVENT, PrintPinStateIsFalling); // 1 -> 0
 }
 
 PROGRAM_LOOP()
