e8d57ad4e92d009eb4a3f5fc88074df38c12e1e6
Natan Lisowski <82398767+natnqweb@users.noreply.github.com>
2022-10-23 21:37:41 +0200

    back to c style pragma once

--- src/macros.h
+++ src/macros.h
@@ -1,4 +1,6 @@
-#pragma once
+#ifndef MACROS_H
+#define MACROS_H
+
 #ifdef DEBUG // DEBUG
 #define LOGLN(X) Serial.println(X)
 #define LOG(X) Serial.print(X)
@@ -38,3 +40,5 @@ void RunInLoop()
 
 #define TURN_OFF_EVENT_OS() ShutDownEventOS()
 #define TURN_ON_EVENT_OS() TurnOnEventOS()
+
+#endif // MACROS_H
