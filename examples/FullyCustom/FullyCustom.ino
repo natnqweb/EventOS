f80d7c9ff7bc3509227676c430d72d9f60fabc4a
Natan Lisowski <82398767+natnqweb@users.noreply.github.com>
2022-11-11 14:00:48 +0100

    updates
    example update and lib prop

--- examples/FullyCustom/FullyCustom.ino
+++ examples/FullyCustom/FullyCustom.ino
@@ -1,7 +1,11 @@
 #define DEBUG
 #include <EventOS.h>
-//for example Custom mapping pins in esp8266
+// for this example Custom mapping pins in esp8266
+// you can custom map any board as long as it is supported by arduino framework
+// esp32 , esp8266 .. all arduino.. raspberrypi .. stm ... etc... 
 
+
+// define your custom virtual pins to use them later in program
 enum CustomVirtualPins : PinType
 {
     VIRTUAL_PIN1,
@@ -9,29 +13,39 @@ enum CustomVirtualPins : PinType
     TOTAL_NUMBER_OF_CUSTOM_PINS
 };
 
+//map real pins and virtual pins together
 PinMap customPinMap[TOTAL_NUMBER_OF_CUSTOM_PINS]
 {
     // MERGE_PINS(REAL ACTUAL BOARD PIN,  YOUR VIRTUALPIN)
+    //* REAL ACTUAL BOARD PIN - pin that you pass for example to digitalRead(REAL ACTUAL BOARD PIN)
     MERGE_PINS(5, VIRTUAL_PIN1)
     MERGE_PINS(4, VIRTUAL_PIN2)
 };
 
+// define PinEvent array it hold your events
 PinEvent customEvents[TOTAL_NUMBER_OF_CUSTOM_PINS]{};
 
 void VPin1FallingEvent()
 {
-    LOGLN("pin 1 state changed from 1 to 0");
+    LOG("pin 1 state changed from 1 to ");
+    LOGLN(GetPinState(VIRTUAL_PIN1));
 }
 
 void VPin2FallingEvent()
 {
-    LOGLN("pin 2 state changed from 1 to 0");
+    LOG("pin 2 state changed from 1 to ");
+    LOGLN(GetPinState(VIRTUAL_PIN2));
 }
-
+// OVERRIDE DEFAULT PROGRAM_SETUP
 __OVERRIDE void PROGRAM_SETUP(115200)
 {
+    // this ovverrides all default events with custom events
     ChangeEvents(customPinMap, customEvents, TOTAL_NUMBER_OF_CUSTOM_PINS);
+    // Init PinEvents after you changed to your custom events
     InitPinEvents();
+
+    //USER CODE
+    //HERE ...
     AddEventListener(VIRTUAL_PIN1, ON_FALLING_EDGE_EVENT, VPin1FallingEvent);
     AddEventListener(VIRTUAL_PIN2, ON_FALLING_EDGE_EVENT, VPin2FallingEvent);
     LOGLN("program started");
